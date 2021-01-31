19d08ef36fd6efa98ee7490d1d800f56e843239c
ArfanBot <78249033+ArfanBot@users.noreply.github.com>
2021-01-31 07:35:18 +0700

    Owner

--- database/menu/owner.js
+++ database/menu/owner.js
@@ -1,1 +1,26 @@
 
+const owner = (prefix, botName, ownerName) => {
+        return `
+「 *${botName}* 」
+
+◪ *INFO*
+  ❏ Prefix: 「  ${prefix}  」
+  ❏ Creator: ${ownerName}
+  ❏ Version: 0.0.4
+◪ *ABOUT*
+  │
+  ├─ ❏ ${prefix}info
+  ├─ ❏ ${prefix}blocklist
+  ├─ ❏ ${prefix}chatlist
+  ├─ ❏ ${prefix}ping
+  └─ ❏ ${prefix}bugreport
+◪ *OWNER*
+  │
+  ├─ ❏ ${prefix}setprefix
+  ├─ ❏ ${prefix}block
+  ├─ ❏ ${prefix}bc
+  ├─ ❏ ${prefix}bcgc
+  ├─ ❏ ${prefix}clone
+  └─ ❏ ${prefix}clearall`
+}
+exports.owner = owner
